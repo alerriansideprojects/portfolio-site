6b4acbef53552f539c2abd085632353ce94304c9
alerrian <steven.a.anderson2012@gmail.com>
2024-12-06 21:19:03 -0600

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Testimonials from "./pages/Testimonials";
 import Contact from "./pages/Contact";
-// import Header from "./pages/Header";
-// import Footer from "./pages/Footer";
 
 export default function App() {
   return (
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import Header from './Header';
 import Footer from './Footer';
 
 export default function Contact() {
     return (
         <div className="about-container">
-            {/* <Header /> */}
             <Footer />
         </div>
     );
